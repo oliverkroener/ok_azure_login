e9d18eb808d39cfffe7fb1db9b95562dfa99f50e
Oliver Kroener <ok@oliver-kroener>
2026-02-12 21:52:22 +0100

    [TASK] Fixed some issues

--- Classes/LoginProvider/AzureLoginProvider.php
+++ Classes/LoginProvider/AzureLoginProvider.php
@@ -6,11 +6,10 @@ namespace OliverKroener\OkAzureLogin\LoginProvider;
 
 use OliverKroener\OkAzureLogin\Domain\Repository\AzureConfigurationRepository;
 use OliverKroener\OkAzureLogin\Service\AzureOAuthService;
-use Psr\Http\Message\ServerRequestInterface;
 use TYPO3\CMS\Backend\Controller\LoginController;
 use TYPO3\CMS\Backend\LoginProvider\LoginProviderInterface;
 use TYPO3\CMS\Core\Page\PageRenderer;
-use TYPO3\CMS\Core\View\ViewInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Fluid\View\StandaloneView;
 
 /**
@@ -18,40 +17,30 @@ use TYPO3\CMS\Fluid\View\StandaloneView;
  * on the TYPO3 backend login screen.
  *
  * Each configured backend config gets its own button.
- * Implements render() for TYPO3 v12 and modifyView() for v13+.
+ * Resolves dependencies from DI container for TYPO3 v11 compat (makeInstance).
  */
 class AzureLoginProvider implements LoginProviderInterface
 {
-    public function __construct(
-        private readonly AzureOAuthService $azureOAuthService,
-        private readonly AzureConfigurationRepository $configurationRepository,
-    ) {}
+    private AzureOAuthService $azureOAuthService;
+    private AzureConfigurationRepository $configurationRepository;
 
-    /**
-     * TYPO3 v12 compatibility: render the login form using StandaloneView.
-     */
+    public function __construct(
+        ?AzureOAuthService $azureOAuthService = null,
+        ?AzureConfigurationRepository $configurationRepository = null,
+    ) {
+        $container = GeneralUtility::getContainer();
+        $this->azureOAuthService = $azureOAuthService
+            ?? $container->get(AzureOAuthService::class);
+        $this->configurationRepository = $configurationRepository
+            ?? $container->get(AzureConfigurationRepository::class);
+    }
+
     public function render(StandaloneView $view, PageRenderer $pageRenderer, LoginController $loginController): void
     {
         $view->assign('azureLogins', $this->collectBackendLogins());
         $view->setTemplatePathAndFilename(
             'EXT:ok_azure_login/Resources/Private/Templates/Login/AzureLoginForm.html'
         );
-    }
-
-    /**
-     * TYPO3 v13+: modify the view and return the template identifier.
-     */
-    public function modifyView(ServerRequestInterface $request, ViewInterface $view): string
-    {
-        $view->assign('azureLogins', $this->collectBackendLogins());
-
-        // Add extension template paths so the login form template can be resolved
-        $templatePaths = $view->getRenderingContext()->getTemplatePaths();
-        $currentPaths = $templatePaths->getTemplateRootPaths();
-        $currentPaths[] = 'EXT:ok_azure_login/Resources/Private/Templates';
-        $templatePaths->setTemplateRootPaths($currentPaths);
-
-        return 'Login/AzureLoginForm';
     }
 
     /**
